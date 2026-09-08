ae020750ca5cac6b2b4ab25cda5722070b598183
sanneflak-lang <sanneflak@gmail.com>
2026-09-08 21:25:33 +0200

    2

--- index.js
+++ index.js
@@ -59,8 +59,34 @@ function createHero() {
     `;
 }
 
+function createAbout() {
+  return `
+        <section class="about">
+
+            <div class="about-text">
+                <h2>MORE THAN A<br>HORSE.<br>A BLOODLINE.</h2>
+
+                <p>
+                    At Equoria, we believe every exceptional horse has a story.
+                    From generations of distinguished bloodlines to future
+                    champions, every detail matters.
+                </p>
+            </div>
+
+            <div class="about-image">
+                <img 
+                    src="./assets/img/pigeoghest.png"
+                    alt="Horse and rider"
+                >
+            </div>
+
+        </section>
+    `;
+}
+
 
 app.innerHTML = `
     ${createHeader()}
     ${createHero()}
+    ${createAbout()}
 `;
